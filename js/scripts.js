3d7b1a9fe7f67a7448db0d837d34bad51367786e
jin camou and Sean Pierce <jin@>
2017-03-21 12:04:59 -0700

    created webpage for JS terms and definitions, add CSS to stylesheet, add JS to scripts.js for first two terms and definitions

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#js-but").click(function() {
+  $(".def").addClass("hide");
+  $("#js-def").addClass("show");
+  });
+  $("#op-but").click(function() {
+  $(".def").addClass("hide");
+  $("#op-def").addClass("show");
+  });
+});
