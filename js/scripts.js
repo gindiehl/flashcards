a7fc369d66ba1c8ef042e0595a86f9baf561213f
jin camou and Sean Pierce <jin@>
2017-03-21 14:05:50 -0700

    Add jQuery that uses this to concatenate an ID value relative to a terms definition.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
-  $("#js-but").click(function() {
-  $(".def").addClass("hide");
-  $("#js-def").addClass("show");
+  // $("#js-but").click(function() {
+  // $(".def").removeClass("show");
+  // $("#js-def").addClass("show");
+  // });
+  // $("#op-but").click(function() {
+  // $(".def").removeClass("show");
+  // $("#op-def").addClass("show");
+  // });
+
+  $("li").click(function() {
+    // create variable to get the id value of this (whatever it is that we clicked on)
+    var thisTerm = $(this).attr('id');
+    // log progress
+    console.log(thisTerm);
+
+    // remove last 4 characters from variable name "-but"
+    thisTerm = thisTerm.slice(0, -4);
+    // log progress
+    console.log(thisTerm);
+
+    // remove the show class from ALL elements with a class ".def"
+    $(".def").removeClass("show");
+
+    // create variable to target a specific definition using the trimmed id from the clicked list item
+    var thisDef = "#" + thisTerm + "-def";
+    // log progress
+    console.log(thisDef);
+
+    // target definition element with our concatenated id
+    $(thisDef).addClass("show");
   });
-  $("#op-but").click(function() {
-  $(".def").addClass("hide");
-  $("#op-def").addClass("show");
-  });
+
 });
